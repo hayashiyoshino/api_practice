70d097703aaa00e22c120726557428e64021357c
林佳志乃 <twinklestaryoshino@gmail.com>
2019-09-27 15:14:08 +0900

    hello vueを表示させた

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'homes/index'
+  root to: "homes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
       resources :posts
       resources :comments
     end
-  end
+	end
 end
