72dc5ebe68c8635e05b2ba46a87809b31af00ccd
林佳志乃 <twinklestaryoshino@gmail.com>
2019-09-27 13:47:35 +0900

    api/v1/commentsコントローラ作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_063202) do
+ActiveRecord::Schema.define(version: 2019_09_26_074727) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
