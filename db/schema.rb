7abebae9894a7a8e2ff66599b985e4205f3051e0
林佳志乃 <twinklestaryoshino@gmail.com>
2019-09-28 19:31:34 +0900

    bookモデルとテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_074727) do
+ActiveRecord::Schema.define(version: 2019_09_28_103053) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.string "publisher"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
