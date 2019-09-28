2f5c38f775e280d3dc1b79e86ef46c8ee0b77cba
林佳志乃 <twinklestaryoshino@gmail.com>
2019-09-28 18:32:33 +0900

    apiで使うのはshowのみに変更

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,36 +2,36 @@ class Api::V1::CommentsController < ApplicationController
   protect_from_forgery
   before_action :set_comment, only: [:show, :update, :destroy]
 
-  def index
-    comments = Comment.all
-    render json: { status: 'SUCCESS', message: 'Loaded comments', data: comments }
-  end
+  # def index
+  #   comments = Comment.all
+  #   render json: { status: 'SUCCESS', message: 'Loaded comments', data: comments }
+  # end
 
   def show
     render json: { status: 'SUCCESS', message: 'Loaded the comment', data: @comment }
   end
 
-  def create
-    comment = Comment.new(comment_params)
-    if comment.save
-      render json: { status: 'SUCCESS', data: comment }
-    else
-      render json: { status: 'ERROR', data: comment.errors }
-    end
-  end
-
-  def destroy
-    @comment.destroy
-    render json: { status: 'SUCCESS', message: 'Deleted the comment', data: @comment }
-  end
-
-  def update
-    if @comment.update(comment_params)
-      render json: { status: 'SUCCESS', message: 'Updated the comment', data: @comment }
-    else
-      render json: { status: 'ERROR', message: 'Not updated', data: @comment.errors }
-    end
-  end
+  # def create
+  #   comment = Comment.new(comment_params)
+  #   if comment.save
+  #     render json: { status: 'SUCCESS', data: comment }
+  #   else
+  #     render json: { status: 'ERROR', data: comment.errors }
+  #   end
+  # end
+  #
+  # def destroy
+  #   @comment.destroy
+  #   render json: { status: 'SUCCESS', message: 'Deleted the comment', data: @comment }
+  # end
+  #
+  # def update
+  #   if @comment.update(comment_params)
+  #     render json: { status: 'SUCCESS', message: 'Updated the comment', data: @comment }
+  #   else
+  #     render json: { status: 'ERROR', message: 'Not updated', data: @comment.errors }
+  #   end
+  # end
 
   private
   def set_comment
---
+++ app/javascript/packs/components/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-if="errors.length" class="error-box">
+      <ul>
+        <li v-for="error in errors" class="errors-list">{{ error }}</li>
+      </ul>
+    </div>
+    <div class="row justify-content-center search-box">
+      <div class="col-offset-2 col-sm-8 col-offset-2">
+        <from @submit.prevent="exec">
+          <div class="input-group">
+            <input v-model="area" class="form-control text-center" placeholder="キーワード">
+            <span class="input-group-addon btn"><button type="submit">検索</button></span>
+          </div>
+        </from>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div v-for="shop in shops" v-if="shops" v-bind:id="'row_shop_' + shop.id">
+        <div class="shop-list">
+          <a :href="shop.url" target="_blank">
+            {{shop.name}}
+            <img :src="shop.image_url.shop_image1">
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
