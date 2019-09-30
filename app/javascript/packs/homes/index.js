1c9bbb8a0e9fff0dba761b50502e727136dfe8a2
林佳志乃 <twinklestaryoshino@gmail.com>
2019-09-30 13:00:54 +0900

    vueでwatchを使ってみた

---
+++ app/javascript/packs/homes/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue/dist/vue.esm';
+import axios from 'axios';
+
+new Vue({
+  el: '#unit-conversion',
+  data: {
+    km: 0,
+    m: 0,
+    mm:0
+  },
+  watch: {
+    km: function(value) {
+      this.km = value
+      this.m = value * 1000
+      this.mm = value * 1000 * 1000
+    },
+    m: function(value) {
+      this.km = value / 1000
+      this.m = value
+      this.mm = value * 1000
+    },
+    mm: function(value) {
+      this.km = value / 1000 /1000
+      this.m = value / 1000
+      this.mm = value
+    }
+  }
+})
